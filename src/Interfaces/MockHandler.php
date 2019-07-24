d822a2159a63cb5784e2a511aabf3bd916474fe7
Adam Kelso <kelso.adam@gmail.com>
2019-07-24 08:16:43 -0400

    Plane ride to Laracon

--- src/Interfaces/MockHandler.php
+++ src/Interfaces/MockHandler.php
@@ -4,5 +4,20 @@ namespace BlastCloud\Chassis\Interfaces;
 
 interface MockHandler extends \Countable
 {
-    public function append($response);
+    /**
+    * Add a mock response to the queue of repsonses
+    * to return from the client.
+    *
+    * @param mixed $response
+    * @return void
+    */
+    public function append($response): void;
+    
+    /**
+    * Return the total number of responses currently
+    * in the mock queue.
+    *
+    * @return int
+    */
+    public function count(): int;
 }
